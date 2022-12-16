37b4aa5c6750fefb6b9c079696c3bbd09ff4cbd8
shawncocklin <shawncocklin@gmail.com>
2022-12-16 12:42:00 -0500

    contact page done

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,78 @@
 import React from 'react'
 import Header from '../components/Header'
+import { DataGrid, GridToolbar } from '@mui/x-data-grid'
+import { tokens } from '../theme'
+import { useTheme, Box } from '@mui/material'
+
+import { mockDataContacts } from '../data/mockData'
+
 export default function Contacts() {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
+  const rows = [...mockDataContacts]
+  const columns = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'registrarId', headerName: 'Registrar ID' },
+    { field: 'name', headerName: 'Name', flex: 1, cellClassName: 'name-col' },
+    {
+      field: 'age',
+      headerName: 'Age',
+      type: 'number',
+      headerAlign: 'left',
+      align: 'left',
+    },
+    { field: 'phone', headerName: 'Phone', flex: 1 },
+    { field: 'email', headerName: 'Email', flex: 1 },
+    { field: 'address', headerName: 'Address', flex: 1 },
+    { field: 'city', headerName: 'City', flex: 1 },
+    { field: 'zipCode', headerName: 'Zip Code', flex: 1 },
+  ]
   return (
-    <div className="ml-6 mt-4">
+    <div className="m-6 h-full flex flex-col">
       <Header
         title="Contacts"
         subtitle="List of Contacts for Future Reference"
       />
+      <Box
+        className="mt-8"
+        height="75vh"
+        sx={{
+          '& .MuiDataGrid-root': {
+            border: 'none',
+          },
+          '& .MuiButton-text': {
+            color: colors.grey[100],
+          },
+          '& .MuiDataGrid-columnHeader': {
+            backgroundColor: colors.blueAccent[700],
+          },
+          '& .MuiDataGrid-columnHeaderTitle': {
+            marginLeft: '2px',
+          },
+          '& .MuiDataGrid-columnSeparator': {
+            backgroundColor: colors.blueAccent[700],
+          },
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: colors.primary[400],
+          },
+          ' & .MuiDataGrid-footerContainer': {
+            backgroundColor: colors.blueAccent[700],
+          },
+          '& .MuiDataGrid-row': {
+            backgroundColor: colors.blueAccent[900],
+          },
+          '& .name-col': {
+            color: colors.greenAccent[400],
+          },
+        }}
+      >
+        <DataGrid
+          components={{ Toolbar: GridToolbar }}
+          columns={columns}
+          rows={rows}
+        />
+      </Box>
     </div>
   )
 }
