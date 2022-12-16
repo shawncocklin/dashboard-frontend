c30a049c717af3c9d08148ca34155b0a7192de6c
shawncocklin <shawncocklin@gmail.com>
2022-12-16 12:53:30 -0500

    complete invoice page

--- src/pages/Invoices.jsx
+++ src/pages/Invoices.jsx
@@ -1,13 +1,77 @@
 import React from 'react'
 import Header from '../components/Header'
+import { DataGrid } from '@mui/x-data-grid'
+import { tokens } from '../theme'
+import { useTheme, Box } from '@mui/material'
+
+import { mockDataInvoices } from '../data/mockData'
 
 export default function Invoices() {
+  const theme = useTheme()
+  const colors = tokens(theme.palette.mode)
+
+  const columns = [
+    { field: 'id', headerName: 'ID' },
+    { field: 'name', headerName: 'Name', flex: 1, cellClassName: 'name-col' },
+    { field: 'phone', headerName: 'Phone', flex: 1 },
+    { field: 'email', headerName: 'Email', flex: 1 },
+    {
+      field: 'cost',
+      headerName: 'Cost',
+      type: 'number',
+      headerAlign: 'left',
+      align: 'left',
+      cellClassName: 'name-col',
+      renderCell: ({ row: { cost } }) => {
+        return `$${cost}`
+      },
+    },
+    { field: 'date', headerName: 'Date', flex: 1 },
+  ]
+  const rows = [...mockDataInvoices]
   return (
-    <div className="ml-6 mt-4">
+    <div className="m-6 h-full flex flex-col">
       <Header
         title="Invoices"
         subtitle="List of Invoice Balances"
       />
+      <Box
+        className="mt-8"
+        height="75vh"
+        sx={{
+          '& .MuiDataGrid-root': {
+            border: 'none',
+          },
+
+          '& .MuiDataGrid-columnHeader': {
+            backgroundColor: colors.blueAccent[700],
+          },
+          '& .MuiDataGrid-columnHeaderTitle': {
+            marginLeft: '2px',
+          },
+          '& .MuiDataGrid-columnSeparator': {
+            backgroundColor: colors.blueAccent[700],
+          },
+          '& .MuiDataGrid-virtualScroller': {
+            backgroundColor: colors.primary[400],
+          },
+          ' & .MuiDataGrid-footerContainer': {
+            backgroundColor: colors.blueAccent[700],
+          },
+          '& .MuiDataGrid-row': {
+            backgroundColor: colors.blueAccent[900],
+          },
+          '& .name-col': {
+            color: colors.greenAccent[400],
+          },
+        }}
+      >
+        <DataGrid
+          checkboxSelection={true}
+          columns={columns}
+          rows={rows}
+        />
+      </Box>
     </div>
   )
 }
