4af7d774242ccdb180aeccd85f669faafefac27b
shawncocklin <shawncocklin@gmail.com>
2022-12-23 12:43:25 -0500

    complete geography chart

--- src/pages/Geo.jsx
+++ src/pages/Geo.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import Header from '../components/Header'
+import GeoChart from '../components/GeoChart'
 
 export default function Geo() {
   return (
-    <div className="ml-6 mt-4">
+    <div className="m-6 h-full flex flex-col">
       <Header
         title="Geography"
         subtitle="Simple Geography Chart"
       />
+      <div
+        style={{ height: '75vh' }}
+        className="mt-6 border-solid border-1 border-white rounded-md"
+      >
+        <GeoChart />
+      </div>
     </div>
   )
 }
